eb0b4b8b5c8f1bef027becfce684e5b35ec885c8
tablatronix <spam@rumental.com>
2014-11-23 16:23:15 -0600

    fix page saves

--- admin/template/js/jquery.getsimple.js
+++ admin/template/js/jquery.getsimple.js
@@ -783,14 +783,15 @@ jQuery(document).ready(function () {
 
     // ajaxify edit.php submit
     $('body #editform').on('submit',function(e){
-        e.preventDefault();
         if($('body').hasClass('ajaxsave')){
+        	e.preventDefault();
             if(checkTitle()) ajaxSave().done(ajaxSaveCallback);
             return false;
         } else {
             warnme      = false;
         	pageisdirty = false;
         	return checkTitle();
+        	// return true;
         }
     });
 
@@ -812,7 +813,7 @@ jQuery(document).ready(function () {
         if($.trim($("#post-title").val()).length === 0){
             alert(i18n('CANNOT_SAVE_EMPTY'));
             return false;
-        }
+        } return true;
     }
 
     // init auto save for page edit
